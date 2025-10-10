4589f5070b6dec34d50be73aff07634c85d40f0d
Krishna <krishnamohan2808@gmail.com>
2025-10-10 18:56:27 +0530

    added another serializer, made imports in views.py

--- api/views.py
+++ api/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics,status
 from .models import room
 from .serializers import RoomSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 
 class RoomView(generics.ListAPIView):
     queryset = room.objects.all()
