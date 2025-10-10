f4ccba48ad6102d384169a0c279b9d16fd58bb6b
Krishna <krishnamohan2808@gmail.com>
2025-10-10 18:44:46 +0530

    added routing to joinpage, createroompage from homepage

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import Roomjoinpage from './Roomjoinpage';
 import Createroompage from './Createroompage';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
 
 export default class Homepage extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
-    return <h1>Welcome to the Music Controller Homepage</h1>;
+    return (
+      <Router>
+        <Routes>
+          <Route path="/" element={<p>This is Home page.</p>} />
+          <Route path="/join" element={<Roomjoinpage />} />
+          <Route path="/create" element={<Createroompage />} />
+        </Routes>
+      </Router>
+    );
   }
 }
