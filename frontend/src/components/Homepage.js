7d272a69a9dc90b77c4b59719c915918d3e13621
Krishna <krishnamohan2808@gmail.com>
2025-10-12 11:55:37 +0530

    created a component Room , imported url in hompage for room for routing

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Roomjoinpage from './Roomjoinpage';
 import Createroompage from './Createroompage';
+import Room from './Room';
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Link,
 } from "react-router-dom";
+
 
 export default class Homepage extends Component {
   render() {
@@ -16,6 +18,7 @@ export default class Homepage extends Component {
           <Route path="/" element={<p>This is Home page.</p>} />
           <Route path="/join" element={<Roomjoinpage />} />
           <Route path="/create" element={<Createroompage />} />
+          <Route path = "/room/:roomCode" element={Room} /> 
         </Routes>
       </Router>
     );
