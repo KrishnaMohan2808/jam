6623d8fbccefcd9efb8bf898fc55e8aeaa53e117
Krishna <krishnamohan2808@gmail.com>
2025-10-12 12:05:32 +0530

    added it to urls

--- frontend/src/components/Room.js
+++ frontend/src/components/Room.js
@@ -8,11 +8,13 @@ export default class Room extends Component {
             guestCanPause: false,
             isHost: false,
         };
+        this.roomCode = this.props.match.params.roomCode;
     }
     
     render() {
         return (
             <div>
+                <h3>{this.roomCode}</h3>
                 <p>Votes: {this.state.votesToSkip}</p>
                 <p>Guest Can Pause: {this.state.guestCanPause.toString()}</p>
                 <p>Host: {this.state.isHost.toString()}</p> 
