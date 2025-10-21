28993166df8bae96e57ed56d76e1bfb7a0d82923
Krishna <krishnamohan2808@gmail.com>
2025-10-21 23:51:34 +0530

    changed the update and create buttons working based on the current state

--- frontend/src/components/Createroompage.js
+++ frontend/src/components/Createroompage.js
@@ -1,134 +1,175 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+// Import useNavigate to pass it down
+import { Link, useNavigate } from 'react-router-dom';
 import {
-  Button,
-  Grid,
-  Typography,
-  TextField,
-  FormHelperText,
-  FormControl,
-  Radio,
-  RadioGroup,
-  FormControlLabel
+Button,
+  Grid,
+  Typography,
+  TextField,
+  FormHelperText,
+  FormControl,
+  Radio,
+  RadioGroup,
+  FormControlLabel
 } from '@mui/material';
 
-export default class Createroompage extends Component {
-  static defaultprops = {
-    votesToSkip: 2,
-    guestCanPause: true,
-    update = false,
-    roomCode: null,
-    updateCallback: () => {} 
+class Createroompage extends Component {
+  // 1. FIXED: 'defaultprops' -> 'defaultProps' and 'update = false' -> 'update: false'
+  static defaultProps = {
+    votesToSkip: 2,
+    guestCanPause: true,
+    update: false,
+    roomCode: null,
+    updateCallback: () => {}
+  };
 
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      guestCanPause: this.props.guestCanPause,
+      votesToSkip: this.props.votesToSkip
+    };
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      guestCanPause: this.props.guestCanPause,
-      votesToSkip: this.props.votesToSkip
-    };
+    this.handleRoomButtonPressed = this.handleRoomButtonPressed.bind(this);
+    this.handleVotesChange = this.handleVotesChange.bind(this);
+    this.handleGuestCanPauseChange = this.handleGuestCanPauseChange.bind(this);
+  }
 
-    this.handleRoomButtonPressed = this.handleRoomButtonPressed.bind(this);
-    this.handleVotesChange = this.handleVotesChange.bind(this);
-    this.handleGuestCanPauseChange = this.handleGuestCanPauseChange.bind(this);
-  }
+  handleVotesChange(e) {
+    this.setState({
+      votesToSkip: e.target.value,
+    });
+  }
 
-  handleVotesChange(e) {
-    this.setState({
-      votesToSkip: e.target.value,
+  handleGuestCanPauseChange(e) {
+    this.setState({
+      guestCanPause: e.target.value === "true",
+    });
+  }
+
+  // 2. FIXED: Added 'update' logic
+  handleRoomButtonPressed() {
+// If this.props.update is true, call the update API
+    if (this.props.update) {
+      const requestOptions = {
+        method: "PATCH", // Use PATCH for updates as in your views.py
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          votes_to_skip: this.state.votesToSkip,
+          guest_can_pause: this.state.guestCanPause,
+          code: this.props.roomCode // Send the room code to update
+        }),
+      };
+
+      fetch("/api/update-room/", requestOptions)
+        .then((response) => {
+      if (response.ok) {
+        // Call the callback to close the settings window
+        this.props.updateCallback(); 
+      } else {
+        console.error("Failed to update room");
+      }
     });
-  }
+    } 
+// Otherwise, call the create API
+else {
+      const requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          votes_to_skip: this.state.votesToSkip,
+          guest_can_pause: this.state.guestCanPause,
+        }),
+      };
 
-  handleGuestCanPauseChange(e) {
-    this.setState({
-      guestCanPause: e.target.value === "true",
-    });
-  }
+      fetch("/api/create-room/", requestOptions)
+        .then((response) => response.json())
+        .then((data) => {
+          // Use the navigate prop from the wrapper
+          this.props.navigate("/room/" + data.code);
+        });
+    }
+  }
 
-  handleRoomButtonPressed() {
-    const requestOptions = {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        votes_to_skip: this.state.votesToSkip,
-        guest_can_pause: this.state.guestCanPause,
-      }),
-    };
+  render() {
+    const title = this.props.update ? "Update Room" : "Create A Room";
+    
+    return (
+      <Grid container spacing={1} >
+        <Grid item xs={12} align="center">
+          <Typography component="h4" variant="h4">
+            {title}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <FormControl component="fieldset">
+            <FormHelperText>
+              Guest Control of Playback State
+            </FormHelperText>
+            <RadioGroup
+              row
+              // 3. FIXED: 'defaultValue' -> 'value'
+          // Convert boolean state to string for the RadioGroup
+              value={this.state.guestCanPause.toString()} 
+              onChange={this.handleGuestCanPauseChange}
+            >
+              <FormControlLabel
+                value="true"
+                control={<Radio color="primary" />}
+                label="Play/Pause"
+                labelPlacement="bottom"
+              />
+              <FormControlLabel
+                value="false"
+                control={<Radio color="secondary" />}
+                label="No Control"
+                labelPlacement="bottom"
+              />
+            </RadioGroup>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <FormControl>
+            <TextField
+              required={true}
+              type="number"
+              onChange={this.handleVotesChange}
+              // 3. FIXED: 'defaultValue' -> 'value'
+              value={this.state.votesToSkip} 
+              inputProps={{
+                min: 1,
+                style: { textAlign: "center" },
+              }}
+            />
+            <FormHelperText>
+              Votes Required To Skip Song
+            </FormHelperText>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Button
+            color="primary"
+            variant="contained"
+            onClick={this.handleRoomButtonPressed}
+s          >
+        {/* 4. FIXED: Button text is now dynamic */}
+            {this.props.update ? "Update Room" : "Create A Room"}
+          </Button>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Button color="secondary" variant="contained" to="/" component={Link}>
+            Back
+          </Button>
+        </Grid>
+      </Grid>
+    );
+  }
+}
 
-    fetch("/api/create-room/", requestOptions)
-      .then((response) => response.json())
-      .then((data) => {
-        // Use the navigate prop from the wrapper
-        this.props.navigate("/room/" + data.code);
-      });
-  }
-
-  render() {
-    return (
-      <Grid container spacing={1} >
-        <Grid item xs={12} align="center">
-          <Typography component="h4" variant="h4">
-            Create A Room
-          </Typography>
-        </Grid>
-        <Grid item xs={12} align="center">
-          <FormControl component="fieldset">
-            <FormHelperText>
-              Guest Control of Playback State
-            </FormHelperText>
-            <RadioGroup
-              row
-              defaultValue="true"
-              onChange={this.handleGuestCanPauseChange}
-            >
-              <FormControlLabel
-                value="true"
-                control={<Radio color="primary" />}
-                label="Play/Pause"
-                labelPlacement="bottom"
-              />
-              <FormControlLabel
-                value="false"
-                control={<Radio color="secondary" />}
-                label="No Control"
-                labelPlacement="bottom"
-              />
-            </RadioGroup>
-          </FormControl>
-        </Grid>
-        <Grid item xs={12} align="center">
-          <FormControl>
-            <TextField
-              required={true}
-              type="number"
-              onChange={this.handleVotesChange}
-              defaultValue={this.state.votesToSkip}
-              inputProps={{
-                min: 1,
-                style: { textAlign: "center" },
-              }}
-            />
-            <FormHelperText>
-              Votes Required To Skip Song
-            </FormHelperText>
-          </FormControl>
-        </Grid> {/* Fixed </sGrid> typo here */}
-        <Grid item xs={12} align="center">
-          <Button
-            color="primary"
-            variant="contained"
-            onClick={this.handleRoomButtonPressed}
-          >
-            Create A Room
-          </Button>
-        </Grid>
-        <Grid item xs={12} align="center">
-          <Button color="secondary" variant="contained" to="/" component={Link}>
-            Back
-          </Button>
-        </Grid>
-      </Grid>
-    );
-  }
+// 5. ADDED: Wrapper component to inject 'navigate' prop
+export default function CreateRoomPageWrapper(props) {
+const navigate = useNavigate();
+return (
+<Createroompage {...props} navigate={navigate} />
+);
 }
